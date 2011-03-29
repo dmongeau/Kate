92f6c760968dd589a457671fceb2839beeaa5b06
David Mongeau-Petitpas <dmp@commun.ca>
2011-03-29 00:08:53 -0400

    Added comments in example

--- example/index.php
+++ example/index.php
@@ -1,7 +1,11 @@
 <?php
 
+/*
+ *
+ * Create Zend Db Object
+ *
+ */
 set_include_path(get_include_path().PATH_SEPARATOR.dirname(__FILE__).'/');
-
 
 require_once 'Zend/Db.php';
 $db = Zend_Db::factory('pdo_mysql',array(
@@ -14,15 +18,25 @@ $db->getConnection();
 $db->query("SET NAMES 'utf8'");
 
 
+/*
+ *
+ * Kate example
+ *
+ */
 require 'Item.php';
 Kate::setDefaultDatabase($db);
 
-
+/*
+ * Create new item
+ */
 $Item = new Item();
 $Item->setData(array('name'=>'item 1'));
 $Item->save();
 $item = $Item->fetch();
 
+/*
+ * Get this item
+ */
 $Item = new Item($item['id']);
 var_dump($Item->fetch());
 exit();
